d86f03125a7b4e808436b345077dfe6d93402a13
railken <railken@protonmail.ch>
2019-03-18 03:30:46 +0100

    macroable relationships

--- src/Providers/ActivityServiceProvider.php
+++ src/Providers/ActivityServiceProvider.php
@@ -6,4 +6,19 @@ use Railken\Amethyst\Common\CommonServiceProvider;
 
 class ActivityServiceProvider extends CommonServiceProvider
 {
+    public function boot()
+    {   
+    	\Illuminate\Database\Eloquent\Builder::macro('activity', function () {
+            return $this->getModel()->morphOne(\Railken\Amethyst\Models\Activity::class, 'sourceable');
+        });
+        \Illuminate\Database\Eloquent\Builder::macro('activities', function () {
+            return $this->getModel()->morphMany(\Railken\Amethyst\Models\Activity::class, 'sourceable');
+        });
+        \Illuminate\Database\Eloquent\Builder::macro('activitiable', function () {
+            return $this->getModel()->morphOne(\Railken\Amethyst\Models\Activitiable::class, 'activitiable');
+        });
+        \Illuminate\Database\Eloquent\Builder::macro('activitiables', function () {
+            return $this->getModel()->morphMany(\Railken\Amethyst\Models\Activitiable::class, 'activitiable');
+        });
+    }
 }
