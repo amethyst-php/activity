c45a234a873402e1fc6e686786b03f4e165d97ff
railken <railken@protonmail.ch>
2019-03-20 13:05:53 +0100

    add return type in macros

--- src/Providers/ActivityServiceProvider.php
+++ src/Providers/ActivityServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Railken\Amethyst\Providers;
 
 use Railken\Amethyst\Common\CommonServiceProvider;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class ActivityServiceProvider extends CommonServiceProvider
 {
@@ -13,16 +15,16 @@ class ActivityServiceProvider extends CommonServiceProvider
     {
         parent::boot();
 
-        \Illuminate\Database\Eloquent\Builder::macro('activity', function () {
+        \Illuminate\Database\Eloquent\Builder::macro('activity', function (): MorphOne {
             return $this->getModel()->morphOne(\Railken\Amethyst\Models\Activity::class, 'sourceable');
         });
-        \Illuminate\Database\Eloquent\Builder::macro('activities', function () {
+        \Illuminate\Database\Eloquent\Builder::macro('activities', function (): MorphMany {
             return $this->getModel()->morphMany(\Railken\Amethyst\Models\Activity::class, 'sourceable');
         });
-        \Illuminate\Database\Eloquent\Builder::macro('activitiable', function () {
+        \Illuminate\Database\Eloquent\Builder::macro('activitiable', function (): MorphOne {
             return $this->getModel()->morphOne(\Railken\Amethyst\Models\Activitiable::class, 'activitiable');
         });
-        \Illuminate\Database\Eloquent\Builder::macro('activitiables', function () {
+        \Illuminate\Database\Eloquent\Builder::macro('activitiables', function (): MorphMany {
             return $this->getModel()->morphMany(\Railken\Amethyst\Models\Activitiable::class, 'activitiable');
         });
     }
