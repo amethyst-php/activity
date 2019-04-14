a6ca14eb9539d1edbe684a91c7e575793c8a4115
railken <railken@protonmail.ch>
2019-04-14 17:50:34 +0200

    update dynamic relations

--- src/Providers/ActivityServiceProvider.php
+++ src/Providers/ActivityServiceProvider.php
@@ -15,31 +15,13 @@ class ActivityServiceProvider extends CommonServiceProvider
     {
         parent::boot();
 
-        \Illuminate\Database\Eloquent\Builder::macro('activity', function (): MorphOne {
-            return $this->createMacroMorphRelation(\Railken\Amethyst\Models\Activity::class, 'activity', 'sourceable');
-        });
-
         \Illuminate\Database\Eloquent\Builder::macro('activities', function (): MorphMany {
             return $this->createMacroMorphRelation(\Railken\Amethyst\Models\Activity::class, 'activities', 'sourceable');
         });
 
-        \Illuminate\Database\Eloquent\Builder::macro('activitiable', function (): MorphOne {
-            if (app('amethyst')->validMorphRelation('activitiable', 'activitiable', $this->getModel()->getMorphName())) {
-                return $this->getModel()->morphOne(\Railken\Amethyst\Models\Activitiable::class, 'activitiable');
-            }
 
-            unset(static::$macros['activitiable']);
-
-            return $this->getModel()->activitiable();
-        });
         \Illuminate\Database\Eloquent\Builder::macro('activitiables', function (): MorphMany {
-            if (app('amethyst')->validMorphRelation('activitiable', 'activitiable', $this->getModel()->getMorphName())) {
-                return $this->getModel()->morphMany(\Railken\Amethyst\Models\Activitiable::class, 'activitiable');
-            }
-
-            unset(static::$macros['activitiables']);
-
-            return $this->getModel()->activitiables();
+            return $this->createMacroMorphRelation(\Railken\Amethyst\Models\Activitiable::class, 'activitiables', 'activitiable');
         });
     }
 }
