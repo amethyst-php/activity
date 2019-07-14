655cbb177924fa87dc220515f15e88ac9f6cbc70
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ActivitiableManager.php
+++ src/Managers/ActivitiableManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Activitiable newEntity()
+ * @method \Amethyst\Schemas\ActivitiableSchema getSchema()
+ * @method \Amethyst\Repositories\ActivitiableRepository getRepository()
+ * @method \Amethyst\Serializers\ActivitiableSerializer getSerializer()
+ * @method \Amethyst\Validators\ActivitiableValidator getValidator()
+ * @method \Amethyst\Authorizers\ActivitiableAuthorizer getAuthorizer()
+ */
 class ActivitiableManager extends Manager
 {
     use ConfigurableManager;
--- src/Managers/ActivityManager.php
+++ src/Managers/ActivityManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Activity newEntity()
+ * @method \Amethyst\Schemas\ActivitySchema getSchema()
+ * @method \Amethyst\Repositories\ActivityRepository getRepository()
+ * @method \Amethyst\Serializers\ActivitySerializer getSerializer()
+ * @method \Amethyst\Validators\ActivityValidator getValidator()
+ * @method \Amethyst\Authorizers\ActivityAuthorizer getAuthorizer()
+ */
 class ActivityManager extends Manager
 {
     use ConfigurableManager;
